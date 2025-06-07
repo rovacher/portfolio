cd7e178ab05e2ca769d16888f73513d86f9b2238
rovacher <vacher.ro@gmail.com>
2025-06-07 09:16:17 +0000

    Normalement le menu fonctionne

--- js/menu.js
+++ js/menu.js
@@ -15,12 +15,12 @@ document.addEventListener('DOMContentLoaded', () => {
     <nav id="side-menu">
       <ul>
         <li><a href="/index.html">Accueil</a></li>
-        <li><a href="/about.html">À propos</a></li>
-        <li><a href="/cv.html">CV</a></li>
-        <li><a href="/projets/Projet_Vapeur.html">Application Web pour les jeux vidéo</a></li>
-        <li><a href="/projets/Projet_VR.html">Jeu vidéo pour les enfants</a></li>
-        <li><a href="/projets/LandL.html">Jeu vidéo sur le suicide</a></li>
-        <li><a href="/projets/MLK.html">Jeu vidéo sur la ségrégation aux USA</a></li>
+        <li><a href="/pages/about.html">À propos</a></li>
+        <li><a href="/pages/cv.html">CV</a></li>
+        <li><a href="/pages/projets/Projet_Vapeur.html">Application Web pour les jeux vidéo</a></li>
+        <li><a href="/pages/projets/Projet_VR.html">Jeu vidéo pour les enfants</a></li>
+        <li><a href="/pages/projets/LandL.html">Jeu vidéo sur le suicide</a></li>
+        <li><a href="/pages/projets/MLK.html">Jeu vidéo sur la ségrégation aux USA</a></li>
       </ul>
     </nav>
     <div class="menu-toggle" onclick="toggleMenu()">☰</div>
