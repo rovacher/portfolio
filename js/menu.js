b5c08df73280667ed10c19d74905353e4bce940e
rovacher <vacher.ro@gmail.com>
2025-06-07 08:49:34 +0000

    menu

--- js/menu.js
+++ js/menu.js
@@ -17,10 +17,10 @@ document.addEventListener('DOMContentLoaded', () => {
         <li><a href="/index.html">Accueil</a></li>
         <li><a href="/about.html">À propos</a></li>
         <li><a href="/cv.html">CV</a></li>
-        <li><a href="/projets/projet1.html">Projet 1</a></li>
-        <li><a href="/projets/projet2.html">Projet 2</a></li>
-        <li><a href="/projets/projet3.html">Projet 3</a></li>
-        <li><a href="/projets/projet4.html">Projet 4</a></li>
+        <li><a href="/projets/Projet_Vapeur.html">Application Web pour les jeux vidéo</a></li>
+        <li><a href="/projets/Projet_VR.html">Jeu vidéo pour les enfants</a></li>
+        <li><a href="/projets/LandL.html">Jeu vidéo sur le suicide</a></li>
+        <li><a href="/projets/MLK.html">Jeu vidéo sur la ségrégation aux USA</a></li>
       </ul>
     </nav>
     <div class="menu-toggle" onclick="toggleMenu()">☰</div>
